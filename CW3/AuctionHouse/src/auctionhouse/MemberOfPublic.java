9d7d366c225feec2789b8d2dba79bc3a1fd2f4bd
Dylan Thinnes <dylan.thinnes@protonmail.com>
2018-11-17 14:56:49 +0000

    Create skeleton MemberOfPublic

---
+++ CW3/AuctionHouse/src/auctionhouse/MemberOfPublic.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package auctionhouse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @author djt
+ *
+ */
+public class MemberOfPublic {
+
+    private static Logger logger = Logger.getLogger("auctionhouse");
+    private static final String LS = System.lineSeparator();
+
+    public ArrayList<CatalogueEntry> viewCatalogue () {
+        return null;
+    }
+    
+}
+
