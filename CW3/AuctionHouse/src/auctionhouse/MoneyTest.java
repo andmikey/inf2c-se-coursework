a566ee7f6fe41fbe936dff0cf9c5e9aa95ce8c09
Mikey <31766607+andmikey@users.noreply.github.com>
2018-11-17 13:11:24 +0000

    Add rounding tests, toString tests

--- CW3/AuctionHouse/src/auctionhouse/MoneyTest.java
+++ CW3/AuctionHouse/src/auctionhouse/MoneyTest.java
@@ -51,6 +51,22 @@ public class MoneyTest {
     }
 
     @Test    
+    public void testAddWithRound() {
+        Money val1 = new Money("12");
+        Money val2 = new Money("0.005");
+        Money result = val1.add(val2);
+        assertEquals("12.01", result.toString());
+    }
+    
+    @Test    
+    public void testAddWithoutRound() {
+        Money val1 = new Money("12");
+        Money val2 = new Money("0.004");
+        Money result = val1.add(val2);
+        assertEquals("12.00", result.toString());
+    }
+    
+    @Test    
     public void testSub() {
         Money val1 = new Money("12.34");
         Money val2 = new Money("0.34");
@@ -78,6 +94,22 @@ public class MoneyTest {
     public void testSubVerySmallNumber() {
         Money val1 = new Money("12");
         Money val2 = new Money("0.00005");
+        Money result = val1.subtract(val2);
+        assertEquals("12.00", result.toString());
+    }
+
+    @Test    
+    public void testSubWithRound() {
+        Money val1 = new Money("12");
+        Money val2 = new Money("0.005");
+        Money result = val1.subtract(val2);
+        assertEquals("11.99", result.toString());
+    }
+    
+    @Test    
+    public void testSubWithoutRound() {
+        Money val1 = new Money("12");
+        Money val2 = new Money("0.004");
         Money result = val1.subtract(val2);
         assertEquals("12.00", result.toString());
     }
@@ -121,6 +153,47 @@ public class MoneyTest {
 	Money result = val1.addPercent(percent);
 	assertEquals("10.00", result.toString());
     }
+
+    @Test
+    public void toStringSimple() {
+	String string = "10.00";
+	Money val1 = new Money(string);
+	String result = val1.toString();
+	assertEquals(string, result);
+    }
+    
+    @Test
+    public void toStringWithNoDec() {
+	String string = "10";
+	Money val1 = new Money(string);
+	String result = val1.toString();
+	assertEquals("10.00", result);
+    }
+    
+    @Test
+    public void toStringWithSmallDec() {
+	String string = "10.0005";
+	Money val1 = new Money(string);
+	String result = val1.toString();
+	assertEquals("10.00", result);
+    }
+
+    @Test
+    public void toStringWithRound() {
+	String string = "10.005";
+	Money val1 = new Money(string);
+	String result = val1.toString();
+	assertEquals("10.01", result);
+    }
+
+    @Test
+    public void toStringWithoutRound() {
+	String string = "10.004";
+	Money val1 = new Money(string);
+	String result = val1.toString();
+	assertEquals("10.00", result);
+    }
+    
     /*
      * Put all class modifications above.
      ***********************************************************************
