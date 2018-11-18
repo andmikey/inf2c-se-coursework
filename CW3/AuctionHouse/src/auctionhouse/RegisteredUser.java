aefc8937db84a2f3b448b91ec900c4f857f0e496
Dylan Thinnes <dylan.thinnes@protonmail.com>
2018-11-18 12:24:21 +0000

    Create RegisteredUser superclass, inherit Actor

---
+++ CW3/AuctionHouse/src/auctionhouse/RegisteredUser.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package auctionhouse;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+/**
+ * @author djt
+ *
+ */
+public class RegisteredUser extends Actor {
+
+    private static Logger logger = Logger.getLogger("auctionhouse");
+    private static final String LS = System.lineSeparator();
+
+    private String username;
+
+    public RegisteredUser (String username, String address, AuctionHouse auctionhouse) {
+        super(address, auctionhouse);
+        this.username = username;
+    }
+
+}
+
